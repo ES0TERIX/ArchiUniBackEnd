ab9d592175a1f1be25c89f7d93edb0ec1f64d74f
ES0TERIX <thomas.carre74@gmail.com>
2024-11-04 09:39:22 +0100

    [TD3] : 8.2.3

---
+++ UniversiteDomain/UseCases/ParcoursUseCases/Create/CreateParcoursUseCase.cs
@@ -0,0 +1,35 @@
+using UniversiteDomain.DataAdapters;
+using UniversiteDomain.Entities;
+using UniversiteDomain.Exceptions.ParcoursExceptions;
+
+namespace UniversiteDomain.UseCases.ParcoursUseCases.Create;
+
+public class CreateParcoursUseCase(IParcoursRepository parcoursRepository)
+{
+    public async Task<Parcours> ExecuteAsync(string nomParcours, int anneeFormation)
+    {
+        var parcours = new Parcours{NomParcours = nomParcours, AnneeFormation = anneeFormation};
+        return await ExecuteAsync(parcours);
+    }
+    public async Task<Parcours> ExecuteAsync(Parcours parcours)
+    {
+        await CheckBusinessRules(parcours);
+        Parcours pa = await parcoursRepository.CreateAsync(parcours);
+        parcoursRepository.SaveChangesAsync().Wait();   
+        return pa;
+    }
+    private async Task CheckBusinessRules(Parcours parcours)
+    {
+        ArgumentNullException.ThrowIfNull(parcours);
+        ArgumentNullException.ThrowIfNull(parcours.NomParcours);
+        ArgumentNullException.ThrowIfNull(parcours.AnneeFormation);
+        ArgumentNullException.ThrowIfNull(parcoursRepository);
+        
+        // On recherche un parcours avec le même nom de parcours
+        List<Parcours> existe = await parcoursRepository.FindByConditionAsync(e=>e.NomParcours.Equals(parcours.NomParcours));
+
+        // Si un parcours avec le même nom existe déjà, on lève une exception personnalisée
+        if (existe.Any())
+            throw new DuplicateNomParcoursException(parcours.NomParcours + " - ce nom de parcours est déjà affecté à un parcours");
+    }
+}
